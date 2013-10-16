e1244b103e2ea4468ecda13cfcc972d32388bc5e
Nico "Pogi" Penaredondo <npenaredondo@gmail.com>
2013-10-16 13:44:51 +0800

    set as default for app.php and database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'citylight',
+			'database'  => 'database',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
